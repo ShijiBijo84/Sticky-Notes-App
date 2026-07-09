ff89a1cca51fdccefb7cd9bf1ae9e9ff2bbdd70e
Shiji Bijo <shijib.bijo@gmail.com>
2026-07-09 14:08:06 +0200

    noteboard-style

--- src/components/NotesBoard/NotesBoard.tsx
+++ src/components/NotesBoard/NotesBoard.tsx
@@ -1,29 +1,46 @@
-import Note from "./Note"
-import { NoteProps } from "../../types/notes"
-import { useLocalStorage } from "../../hooks/useLocalStorage"
-import { useNotes } from "../../hooks/useNotes"
-import { useDrag } from "../../hooks/useDrag"
-import { INITIAL_NOTES } from "../../constants/constants"
-import ToolBar from "./ToolBar"
-import { useState } from "react"
 
+import Note from "./Note";
+import { NoteProps } from "../../types/notes";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { useNotes } from "../../hooks/useNotes";
+import { useDrag } from "../../hooks/useDrag";
+import { INITIAL_NOTES } from "../../constants/constants";
+import ToolBar from "./ToolBar";
+import { useState } from "react";
 
 const NotesBoard = () => {
-    const [notes, setNotes] = useLocalStorage<NoteProps[]>('notes', INITIAL_NOTES)
-    const [selectedNoteId, setSelectedNoteId] = useState<number | null>(null)
-    const { addNote, deleteNote, updateNote, moveNote } = useNotes(notes, setNotes)
-    const { startDrag } = useDrag(moveNote, setSelectedNoteId)
+    const [notes, setNotes] = useLocalStorage<NoteProps[]>(
+        "notes",
+        INITIAL_NOTES
+    );
+
+    const [selectedNoteId, setSelectedNoteId] = useState<number | null>(null);
+
+    const { addNote, deleteNote, updateNote, moveNote } = useNotes(
+        notes,
+        setNotes
+    );
+
+    const { startDrag } = useDrag(moveNote, setSelectedNoteId);
 
     return (
-        <div className="notes-board">
-            <ToolBar addNote={addNote} deleteNote={deleteNote} selectedId={selectedNoteId} />
+        <div className="relative h-screen w-screen overflow-hidden bg-gray-200">
+            <ToolBar
+                addNote={addNote}
+                deleteNote={deleteNote}
+                selectedId={selectedNoteId}
+            />
+
             {notes.map((note) => (
-                <Note key={note.id} note={note}
+                <Note
+                    key={note.id}
+                    note={note}
                     updateNote={updateNote}
                     startDrag={startDrag}
                 />
             ))}
         </div>
-    )
-}
-export default NotesBoard
+    );
+};
+
+export default NotesBoard;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/notes.scss";
+import "./styles/index.css";
 import NotesBoard from "./components/NotesBoard/NotesBoard";
 
 createRoot(document.getElementById("root")!).render(
